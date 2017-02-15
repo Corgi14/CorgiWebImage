2236a228f9fc95dcf1ec5505620caf8a4904c57d
Corgi <949952431@qq.com>
2017-02-15 14:51:24 +0800

    no message

---
+++ CorgiWebImageDownloadOperation/CorgiWebImageDownloadOperation/CorgiWebImage/DownloaderOperation.h
@@ -0,0 +1,13 @@
+//
+//  DownloaderOperation.h
+//  CorgiWebImageDownloadOperation
+//
+//  Created by Corgi on 17/2/15.
+//  Copyright © 2017年 Corgi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DownloaderOperation : NSOperation
+
+@end
---
+++ CorgiWebImageDownloadOperation/CorgiWebImageDownloadOperation/CorgiWebImage/DownloaderOperation.m
@@ -0,0 +1,13 @@
+//
+//  DownloaderOperation.m
+//  CorgiWebImageDownloadOperation
+//
+//  Created by Corgi on 17/2/15.
+//  Copyright © 2017年 Corgi. All rights reserved.
+//
+
+#import "DownloaderOperation.h"
+
+@implementation DownloaderOperation
+
+@end
